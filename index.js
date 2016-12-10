30f0ff732d205313ea6bb40265092990eece5e41
Jesse Tane <jesse.tane@gmail.com>
2016-12-09 19:51:56 -0500

    rewrite, add some http features

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ var url = require('url')
 var diff = require('object-diff')
 var isTls = require('is-tls-client-hello')
 var extractSni = require('sni')
-var isHttp = /^[^ ]+ [^ ]+ HTTP\/1\.1$/m
+var isHttp = /^.+ .+ HTTP\/1\.1$/m
 var extractHostHeader = /\r\nhost: (.+?)(?:\r|$)/i
+var basicAuth = require('basic-auth')
 
-module.exports = class Terminus extends EventEmitter {
+module.exports = class Splicer extends EventEmitter {
   constructor (opts = {}) {
     super()
 
@@ -37,12 +38,6 @@ module.exports = class Terminus extends EventEmitter {
 
     // outward facing tcp listeners
     this._tcpListeners = {}
-
-    // tcp listener for handling domain validation requests from ACME CAs
-    this.acmeValidationPort = opts.acmeValidationPort
-    if (this.acmeValidationPort) {
-      this._tcpListeners[this.acmeValidationPort] = this._createTcpListener(this.acmeValidationPort)
-    }
 
     // tls terminator
     this._tlsServer = new tls.Server({
@@ -84,6 +79,7 @@ module.exports = class Terminus extends EventEmitter {
     var oldPorts = diff(newData.ports || {}, oldData.ports || {})
     var newPorts = diff(oldData.ports || {}, newData.ports || {})
     for (var port in oldPorts) {
+      if (port === '*') continue
       var listener = this._tcpListeners[port]
       if (listener && --listener.apps === 0) {
         delete this._tcpListeners[port]
@@ -92,6 +88,7 @@ module.exports = class Terminus extends EventEmitter {
       }
     }
     for (port in newPorts) {
+      if (port === '*') continue
       listener = this._tcpListeners[port]
       if (listener) {
         listener.apps++
@@ -99,6 +96,9 @@ module.exports = class Terminus extends EventEmitter {
         this._tcpListeners[port] = this._createTcpListener(port)
       }
     }
+    newData.tls = newData.tls && typeof newData.tls === 'object'
+      ? newData.tls
+      : { front: newData.tls || false, back: false }
   }
 
   SNICallback (name, cb) {
@@ -116,12 +116,12 @@ module.exports = class Terminus extends EventEmitter {
     }
   }
 
-  isDomainValidationRequest (pathname) {
+  isAcmeHttpChallenge (pathname) {
     return false
   }
 
-  setChallenge (key, value) {
-    return false
+  setAcmeChallenge (key, value) {
+    // user may implement
   }
 
   balanceLoad (app) {
@@ -141,10 +141,6 @@ module.exports = class Terminus extends EventEmitter {
 
   _ontcpConnection (socket) {
     this.emit('connection', socket)
-    socket.setTimeout(this.timeout, () => {
-      console.log('TIMEOUT')
-      socket.destroy()
-    })
     socket.once('readable', () => {
       var firstPacket = socket.read() || new Buffer(0)
       if (isTls(firstPacket)) {
@@ -170,9 +166,17 @@ module.exports = class Terminus extends EventEmitter {
     }
     socket.app = app
     socket.unshift(firstPacket)
-    if (app.tls) {
+    if (app.tls.front) {
       this._tlsServer.emit('connection', socket)
     } else {
+      socket.setNoDelay(true)
+      socket.on('error', err => {
+        // console.log('SECURE PASSTHROUGH ERROR', err)
+      })
+      socket.setTimeout(this.timeout, () => {
+        // console.log('SECURE PASSTHROUGH TIMEOUT')
+        socket.destroy()
+      })
       this._proxy(socket)
     }
   }
@@ -185,12 +189,32 @@ module.exports = class Terminus extends EventEmitter {
       socket.end('HTTP/1.1 404 Not Found\r\n\r\nnot found')
       return
     }
-    if (app.tls) {
-      this._checkDomainValidation(socket, headers, host[1])
+    if (app.tls.front) {
+      var pathname = headers.slice(headers.indexOf(' ') + 1)
+      pathname = pathname.slice(0, pathname.indexOf(' '))
+      if (this.isAcmeHttpChallenge(pathname)) {
+        var proof = this.challenges[pathname]
+        if (proof) {
+          this.setAcmeChallenge(pathname, null)
+          socket.end(`HTTP/1.1 200 OK\r\n\r\n${proof}`)
+        } else {
+          socket.end('HTTP/1.1 404 Not Found\r\n\r\nnot found')
+        }
+      } else {
+        socket.end(`HTTP/1.1 302 Found\r\nLocation: https://${host[1]}${pathname}\r\n\r\n`)
+      }
       return
     }
     socket.app = app
     socket.unshift(firstPacket)
+    socket.setNoDelay(true)
+    socket.on('error', err => {
+      // console.log('INSECURE HTTP ERROR', err)
+    })
+    socket.setTimeout(this.timeout, () => {
+      // console.log('INSECURE HTTP TIMEOUT')
+      socket.destroy()
+    })
     if (app.http) {
       this._httpServer.emit('connection', socket)
     } else {
@@ -198,29 +222,16 @@ module.exports = class Terminus extends EventEmitter {
     }
   }
 
-  _checkDomainValidation (socket, headers, host) {
-    var pathname = headers.slice(headers.indexOf(' ') + 1)
-    pathname = pathname.slice(0, pathname.indexOf(' '))
-    if (this.isDomainValidationRequest(pathname)) {
-      var proof = this.challenges[pathname]
-      if (proof) {
-        socket.end(`HTTP/1.1 200 OK\r\n\r\n${proof}`)
-        this.setChallenge(pathname, null)
-        return
-      } else {
-        socket.end('HTTP/1.1 404 Not Found\r\n\r\nnot found')
-      }
-    } else {
-      socket.end(`HTTP/1.1 302 Found\r\nLocation: https://${host}${pathname}\r\n\r\n`)
-    }
-  }
-
   _ontlsConnectionSecure (socket) {
+    var app = socket.app = socket._handle._parentWrap.app // this will probably break
+    socket.setNoDelay(true)
     socket.on('error', err => {
-      console.log('TLS ERROR', err)
+      // console.log('SECURE ERROR', err)
+    })
+    socket.setTimeout(this.timeout, () => {
+      // console.log('SECURE TIMEOUT')
       socket.destroy()
     })
-    var app = socket.app = socket._handle._parentWrap.app // this will probably break
     if (app.http) {
       this._httpServer.emit('connection', socket)
     } else {
@@ -230,24 +241,42 @@ module.exports = class Terminus extends EventEmitter {
 
   _onhttpRequest (req, res) {
     var socket = req.socket
-    var app = socket.app
+    var app = this.apps[socket.app.id]
+    if (!app) {
+      res.statusCode = 404
+      res.end('not found')
+      return
+    }
     var host = req.headers.host || ''
     var parts = host.split(':')
     var name = parts[0]
     var port = parts[1]
     if (app.http.cname && name !== app.http.cname) {
-      var protocol = app.tls ? 'https' : 'http'
+      var protocol = app.tls.front ? 'https' : 'http'
       port = port ? ':' + port : ''
       res.setHeader('location', `${protocol}://${app.http.cname}${port}${req.url}`)
       res.statusCode = 302
       res.end()
       return
     }
-    var pre = app.http.pre
+    var httpAuth = app.http.auth
+    if (httpAuth) {
+      var auth = basicAuth(req)
+      if (!auth || auth.name !== httpAuth.username || auth.pass !== httpAuth.password) {
+        res.statusCode = 401
+        res.setHeader('WWW-Authenticate', `Basic realm="${name}"`)
+        res.end('access denied')
+        return
+      }
+    }
+    var { pre, post } = app.http
+    if (pre || post) {
+      req.parsedUrl = url.parse(req.url, true)
+    }
     var dest = app
     if (pre) {
       if (typeof pre === 'string') {
-        pre = app.http.pre = (new Function(pre))()
+        pre = app.http.pre = (new Function('require', pre))(require)
       }
       var redirect = pre(req, app)
       if (typeof redirect === 'string') {
@@ -256,29 +285,21 @@ module.exports = class Terminus extends EventEmitter {
     }
     var machine = dest && dest.machines && this.balanceLoad(dest)
     var upstreamAddress = machine && machine.address
-    var upstreamPort = dest && dest.ports && dest.ports[socket.localPort]
+    var upstreamPort = dest && dest.ports && (dest.ports[socket.localPort] || dest.ports['*'])
     if (!upstreamAddress || !upstreamPort) {
       res.statusCode = 503
       res.end('service unavailable')
       return
     }
-    var upstream = url.parse(upstreamAddress)
-    if (upstream.protocol === null) {
-      upstream = {
-        protocol: 'http:',
-        hostname: upstreamAddress
-      }
-    }
-    var iface = (upstream.protocol === 'https:' ? https : http)
+    var transport = (dest.tls.back ? https : http)
     var opts = {
-      protocol: upstream.protocol,
-      hostname: upstream.hostname,
+      hostname: upstreamAddress,
       port: upstreamPort,
       path: req.url,
       agent: this._httpAgent,
       headers: req.headers
     }
-    var uReq = iface.request(opts)
+    var uReq = transport.request(opts)
     uReq.on('error', err => {
       res.statusCode = 503
       res.end('service unavailable')
@@ -287,10 +308,9 @@ module.exports = class Terminus extends EventEmitter {
     uReq.on('upgrade', onresponse)
     req.pipe(uReq)
     function onresponse (uRes, uSocket) {
-      var post = app.http.post
       if (post) {
         if (typeof post === 'string') {
-          post = app.http.post = (new Function(post))()
+          post = app.http.post = (new Function('require', post))(require)
         }
         post(uRes, app)
       }
@@ -349,9 +369,20 @@ module.exports = class Terminus extends EventEmitter {
     var app = socket.app
     var machine = this.balanceLoad(app)
     var upstreamPort = app.ports[socket.localPort]
+    if (!upstreamPort) {
+      upstreamPort = app.ports['*']
+    }
     var upstreamAddress = machine && machine.address
     if (upstreamAddress && upstreamPort) {
-      var upstream = net.connect(upstreamPort, upstreamAddress)
+      var transport = app.tls.back ? tls : net
+      var upstream = transport.connect({
+        host: upstreamAddress,
+        port: upstreamPort
+      })
+      upstream.setNoDelay(true)
+      upstream.on('error', err => {
+        // console.log('UPSTREAM ERROR', err)
+      })
       socket.pipe(upstream).pipe(socket)
     } else {
       socket.destroy()
